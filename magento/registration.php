166588edfbd3a1041e419bab38e182d7f4ee491f
Flafi <laaszlo.gaal@gmail.com>
2021-03-30 10:17:12 +0200

    initial commit

---
+++ magento/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/scandipwa/cart_progress',
+    __DIR__
+);
